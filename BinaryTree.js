b5a48cded6f47b04f23f249982488f2a7c6e0ba4
Nogiback <peter.toan.do@gmail.com>
2023-10-17 18:04:27 -0400

    Added insert, delete, find, and levelOrder functions to BinaryTree class

--- BinaryTree.js
+++ BinaryTree.js
@@ -10,18 +10,110 @@ class BinaryTree {
     return sortedArray;
   }
 
+  #findMin(node) {
+    let min = node.data;
+    while (node.left !== null) {
+      min = node.left.data;
+      node = node.left;
+    }
+    return min;
+  }
+
   buildTree(array) {
     let sortedArray = this.#sortArray(array);
     if (sortedArray.length === 0) {
       return null;
     }
     const midPoint = Math.floor(sortedArray.length / 2);
-    const root = new Node(
-      sortedArray[midPoint],
-      this.buildTree(sortedArray.slice(0, midPoint)),
-      this.buildTree(sortedArray.slice(midPoint + 1)),
-    );
+    const root = new Node(sortedArray[midPoint]);
+    root.left = this.buildTree(sortedArray.slice(0, midPoint));
+    root.right = this.buildTree(sortedArray.slice(midPoint + 1));
     return root;
+  }
+
+  insert(data, node = this.root) {
+    if (node === null) {
+      node = new Node(data);
+      return node;
+    }
+    if (node.data === data) {
+      return;
+    }
+
+    if (data < node.data) {
+      node.left = this.insert(data, node.left);
+    } else if (data > node.data) {
+      node.right = this.insert(data, node.right);
+    }
+    return node;
+  }
+
+  delete(data, node = this.root) {
+    if (node === null) {
+      return node;
+    }
+
+    if (node.data > data) {
+      node.left = this.delete(data, node.left);
+    } else if (node.data < data) {
+      node.right = this.delete(data, node.right);
+    } else {
+      // Once root === node to be deleted
+      // If one of the child nodes is empty
+      if (node.left === null) {
+        return node.right;
+      } else if (node.right === null) {
+        return node.left;
+      }
+      // If both child nodes exist
+      node.data = this.#findMin(node.right);
+      node.right = this.delete(node.data, node.right);
+    }
+    return node;
+  }
+
+  find(data, node = this.root) {
+    if (node === null || node.data === data) {
+      return node;
+    }
+
+    if (node.data > data) {
+      return this.find(data, node.left);
+    }
+
+    if (node.data < data) {
+      return this.find(data, node.right);
+    }
+  }
+
+  levelOrder(callbackFn) {
+    if (this.root === null) {
+      return [];
+    }
+    const queue = [this.root];
+    const levelOrderList = [];
+
+    while (queue.length > 0) {
+      let level = [];
+      let size = queue.length;
+      for (let i = 0; i < size; i++) {
+        const node = queue.shift();
+        level.push(node.data);
+        if (node.left !== null) {
+          queue.push(node.left);
+        }
+        if (node.right !== null) {
+          queue.push(node.right);
+        }
+        if (callbackFn) {
+          callbackFn(node);
+        }
+      }
+      levelOrderList.push(level);
+    }
+    if (!callbackFn) {
+      return levelOrderList;
+    }
   }
 
   prettyPrint(node = this.root, prefix = "", isLeft = true) {
@@ -47,3 +139,9 @@ module.exports = BinaryTree;
 const array = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
 let tree = new BinaryTree(array);
 tree.prettyPrint();
+tree.insert(33);
+tree.prettyPrint();
+tree.delete(23);
+tree.prettyPrint();
+tree.find(4);
+console.log(tree.levelOrder());
--- script.js
+++ script.js
@@ -1,1 +1,3 @@
 const BinaryTree = require("./BinaryTree.js");
+
+//Driver script for testing will go here once all functionality is complete in BST
